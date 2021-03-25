7f4da4da7a1dc63ecbfc73ad997154d43c57f080
kimtaejun <kim_taejun@naver.com>
2021-03-25 22:55:33 +0900

    Modifying the test code(author 수정)

--- src/main/java/Spring/dao/UserDaoTest.java
+++ src/main/java/Spring/dao/UserDaoTest.java
@@ -3,12 +3,12 @@ package Spring.dao;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.dao.support.DaoSupport;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
@@ -17,15 +17,21 @@ import static org.hamcrest.CoreMatchers.is;
 
 
 
+@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = DaoFactory.class)
+@ContextConfiguration(locations = {"/applicationContext.xml"})
 public class UserDaoTest {
     private UserDao userDao;
     private User user1;
     private User user2;
     private User user3;
 
+    //테스 오브젝트가 만들어지고 나면 자동으로 값 주입.
+    @Autowired
+    private ApplicationContext context;
+
     @Before
     public void SetUp() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         this.userDao = context.getBean("makeUserDao", UserDao.class);
 
         user1 = new User("kim", "김", "no1");
@@ -33,20 +39,19 @@ public class UserDaoTest {
         user3 = new User("Lee", "이", "no3");
 
         userDao.deleteAll();
-        assertThat(userDao.getCount(), is(0));
+
 
     }
 
     @After
     public void clearData() throws SQLException, ClassNotFoundException {
         userDao.deleteAll();
-        assertThat(userDao.getCount(), is(0));
+//        assertThat(userDao.getCount(), is(0));
     }
 
 
     @Test
     public void addAndGet() throws SQLException, ClassNotFoundException {
-//        ApplicationContext context =new GenericXmlApplicationContext("applicationContext.xml");
 
         userDao.add(user1);
         userDao.add(user2);
@@ -81,7 +86,7 @@ public class UserDaoTest {
 
         userDao.add(user3);
         assertThat(userDao.getCount(), is(count +1));
-        count = userDao.getCount();
+        
 
     }
 
